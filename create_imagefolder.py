c7314f8ec5f75ea4584af819cf9c2e03d668900f
NIH-MIP <45637019+NIH-MIP@users.noreply.github.com>
2018-12-05 14:07:40 -0500

    Update create_imagefolder.py

--- create_imagefolder.py
+++ create_imagefolder.py
@@ -11,13 +11,10 @@ class CreateImageFolder:
 
     '''
 
-    def __init__(self,path_to_input='',
-                 path_to_output='',
-                 percent=0.2):
+    def __init__(self,path_to_input='',path_to_output='',percent=0.2,neg_lab='neg',pos_lab='pos'):
         '''
         Args:
-            path_to_input: path to the folder containing images.  Expect them to have image extension (i.e. .jpg) and label after final underscore (i.e. tumor1_neg.jpeg would be placed in file 'neg')
-            
+            path_to_input: path to the folder containing images.  Expect them to have image extension (i.e. .jpg) and label after final underscore
             path_to_output: the path where you want your image folder saved
             percent: percent of patients in validation (if you have 100 patients and this value is 0.2, you will have 20 validation patients)
         '''
@@ -25,12 +22,14 @@ class CreateImageFolder:
         self.inpath=path_to_input
         self.outpath=path_to_output
         self.percent=percent
+        self.neg_label=neg_lab
+        self.pos_label=pos_lab
 
     def create_image_folder(self):
         '''creates an image folder'''
 
         #use function to below to make sure all files present in output directory
-        self.make_out_dir()
+        self.make_out_dir(neg_lab=self.neg_label,pos_lab=self.pos_label)
 
         #get all file paths
         all_file_path=pd.Series([os.path.join(self.inpath,file) for file in os.listdir(self.inpath)])
@@ -44,13 +43,13 @@ class CreateImageFolder:
         for state in split.keys():
             state_item=split[state]
             for path in state_item:
-                if path.split('.')[0].split('_')[-1] == 'neg':
-                    shutil.copy2(path,os.path.join(self.outpath,state,'neg'))
-                if path.split('.')[0].split('_')[-1] == 'pos':
-                    shutil.copy2(path, os.path.join(self.outpath, state, 'pos'))
+                if path.split('.')[0].split('_')[-1] == self.neg_label:
+                    shutil.copy2(path,os.path.join(self.outpath,state,self.neg_label))
+                if path.split('.')[0].split('_')[-1] == self.pos_label:
+                    shutil.copy2(path, os.path.join(self.outpath, state, self.pos_label))
 
 
-    def make_out_dir(self):
+    def make_out_dir(self,neg_lab,pos_lab):
         '''
         looks through all the values in the output directory and makes the correct data structure and makes folds
         for all relevant files.
@@ -65,10 +64,10 @@ class CreateImageFolder:
             if not os.path.exists(path_state_dir):
                 os.mkdir(path_state_dir)
                 os.chdir(path_state_dir)
-                if not os.path.exists(os.path.join(path_state_dir,'neg')):
-                    os.mkdir(os.path.join(path_state_dir,'neg'))
-                if not os.path.exists(os.path.join(path_state_dir,'pos')):
-                    os.mkdir(os.path.join(path_state_dir,'pos'))
+                if not os.path.exists(os.path.join(path_state_dir,neg_lab)):
+                    os.mkdir(os.path.join(path_state_dir,neg_lab))
+                if not os.path.exists(os.path.join(path_state_dir,pos_lab)):
+                    os.mkdir(os.path.join(path_state_dir,pos_lab))
 
 
 if __name__=='__main__':
@@ -76,7 +75,9 @@ if __name__=='__main__':
     parser.add_argument('--input_path')
     parser.add_argument('--output_path')
     parser.add_argument('--percent', default=0.2, type=float)
+    parser.add_argument('--neg_lab', default='neg', type=str)
+    parser.add_argument('--pos_lab',default='pos', type=str)
     args = parser.parse_args()
 
-    c=CreateImageFolder(args.input_path,args.output_path,args.percent)
+    c=CreateImageFolder(args.input_path,args.output_path,args.percent,args.neg_lab,args.pos_lab)
     c.create_image_folder()
